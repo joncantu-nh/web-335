8c622c324c1babeaf5aa902b0332a12e9e6d0a1e
JonathanCantu <jcantu@jcantu.com>
2026-06-15 00:02:49 -0400

    Week 2 assignment and exercise.

---
+++ week-2/exercise2-3.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        print(
+            "Author: Jonathan Cantu\n"
+            "Course: WEB335\n"
+            "Assignment: Exercise 2.3 - Python\n\n"
+            "“Tell me and I forget, teach me and I remember, involve me and I learn.” - Benjamin Franklin"
+        )
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
